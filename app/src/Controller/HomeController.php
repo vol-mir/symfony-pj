4d6b3f625851ff52e3d73366d755ba5861e495db
Vladimir Bykovsky <v.bykovsky@syberry.com>
2022-02-17 21:27:46 +0300

    add admin lte

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HomeController
+ * @package App\Controller
+ */
+class HomeController extends AbstractController
+{  
+    /**
+     * Index page
+     *
+     * @Route("/", name="home")
+     * 
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
